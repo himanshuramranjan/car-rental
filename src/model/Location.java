34dbf5dcaed409ea22343dd376e3452c3b03dabb
Himanshuram Ranjan <himanshuramranjan@Himanshurams-MacBook-Air.local>
2025-03-02 22:49:46 +0530

    Location defined for the app

---
+++ src/model/Location.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Location {
+    private String state;
+    private String city;
+    private String address;
+    private int pincode;
+
+    public Location(String state, String city, String address, int pincode) {
+        this.state = state;
+        this.city = city;
+        this.address = address;
+        this.pincode = pincode;
+    }
+}
