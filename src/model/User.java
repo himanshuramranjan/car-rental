390c7a04456d084a7b5928b86e515b585ca50c69
Himanshuram Ranjan <himanshuramranjan@Himanshurams-MacBook-Air.local>
2025-07-06 12:29:22 +0530

    converted to Record

--- src/model/User.java
+++ src/model/User.java
@@ -1,25 +1,3 @@
 package model;
 
-public class User {
-    private int id;
-    private String name;
-    private String drivingLicense;
-
-    public User(int id, String name, String drivingLicense) {
-        this.id = id;
-        this.name = name;
-        this.drivingLicense = drivingLicense;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDrivingLicense() {
-        return drivingLicense;
-    }
-}
+public record User(int id, String name, String drivingLicense) {}
